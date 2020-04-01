af5611ab8ef38832861dcd98f401405cb093a42a
dev5151 <iamdev5151@gmail.com>
2020-04-02 03:15:13 +0530

    Added View Binding

---
+++ app/src/main/java/com/dev5151/dicerollingkotlin/Main2Activity.kt
@@ -0,0 +1,15 @@
+package com.dev5151.dicerollingkotlin
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main2Activity : AppCompatActivity() {
+
+    
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
--- app/src/main/java/com/dev5151/dicerollingkotlin/MainActivity.kt
+++ app/src/main/java/com/dev5151/dicerollingkotlin/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.dev5151.dicerollingkotlin
 
+import android.content.Intent
 import android.graphics.drawable.Animatable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -11,6 +12,8 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.dev5151.dicerollingkotlin.databinding.ActivityMainBinding
 import java.util.*
 import kotlin.concurrent.thread
 
@@ -19,31 +22,41 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var btn: Button
     private lateinit var dice: ImageView
+    private lateinit var tvSwitch: TextView
     private lateinit var rotate: Animation
+
+    private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        btn = findViewById(R.id.btn)
+        /*btn = findViewById(R.id.btn)
         dice = findViewById(R.id.dice)
+        tvSwitch = findViewById(R.id.tv_switch)
         rotate = AnimationUtils.loadAnimation(applicationContext, R.anim.rotate_picture)
-
-        btn.setOnClickListener {
+*/
+        binding.btn.setOnClickListener {
             rollDice()
+        }
+        binding.tvSwitch.setOnClickListener {
+            startActivity(Intent(applicationContext, Main2Activity::class.java))
         }
 
     }
 
     private fun rollDice() {
-        dice.startAnimation(rotate)
+       // binding.apply { binding.dice.startAnimation(rotate) }
+        //binding.invalidateAll();
         when (Random().nextInt(6) + 1) {
-            1 -> dice.setImageResource(R.drawable.dice_1)
-            2 -> dice.setImageResource(R.drawable.dice_2)
-            3 -> dice.setImageResource(R.drawable.dice_3)
-            4 -> dice.setImageResource(R.drawable.dice_4)
-            5 -> dice.setImageResource(R.drawable.dice_5)
-            6 -> dice.setImageResource(R.drawable.dice_6)
+            1 -> binding.dice.setImageResource(R.drawable.dice_1)
+            2 -> binding.dice.setImageResource(R.drawable.dice_2)
+            3 -> binding.dice.setImageResource(R.drawable.dice_3)
+            4 -> binding.dice.setImageResource(R.drawable.dice_4)
+            5 -> binding.dice.setImageResource(R.drawable.dice_5)
+            6 -> binding.dice.setImageResource(R.drawable.dice_6)
             else -> {
                 Toast.makeText(applicationContext, "ERROR OCCURED", Toast.LENGTH_LONG).show()
             }
